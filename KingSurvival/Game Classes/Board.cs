2628019f4fa51490eeb0723ba47c280c9845231b
mrslvts <mrslvts@gmail.com>
2014-07-15 17:58:45 +0300

    extracted the board filling functionality in a method; changed Board class from static to instantiated; other little adjustments

--- KingSurvival/Game Classes/Board.cs
+++ KingSurvival/Game Classes/Board.cs
@@ -1,49 +1,53 @@
 ﻿namespace KingSurvival
 {
-    public static class Board
-    {
-        // The game is played on a standard chess-board of size 8 x 8 cells (half of them - white, the other half - black).
-        public const int BoardSize = 8;
-        private const char EmptyWhiteCell = '+';
-        private const char EmptyBlackCell = '-';
+	internal class Board
+	{
+		// The game is played on a standard chess-board of size 8 x 8 cells (half of them - white, the other half - black).
+		public const int BoardSize = 8;
 
-        private static char[,] gameField;
-        private static char currentCell;
+		private const char EmptyWhiteCell = '+';
+		private const char EmptyBlackCell = '-';
 
-        static Board()
-        {
-            gameField = new char[BoardSize, BoardSize];
+		private readonly char[,] gameField;
 
-            for (int row = 0; row < BoardSize; row++)
-            {
-                for (int col = 0; col < BoardSize; col++)
-                {
-                    // Check if both row and col numbers are even/odd at the same time.
-                    if (row % 2 == col % 2)
-                    {
-                        currentCell = EmptyWhiteCell;
-                    }
-                    else
-                    {
-                        currentCell = EmptyBlackCell;
-                    }
+		internal Board()
+		{
+			gameField = new char[BoardSize, BoardSize];
 
-                    gameField[row, col] = currentCell;
-                }
-            }
-        }
+			FillBoard();
+		}
 
-        // Drawing method just for testing
-        public static void drawBoard()
-        {
-            for (int i = 0; i < BoardSize; i++)
+		private void FillBoard()
+		{
+			for (int row = 0; row < BoardSize; row++)
 			{
-                for (int j = 0; j < BoardSize; j++)
-                {
-                    System.Console.Write(gameField[i, j]);
-                }
-                System.Console.WriteLine();
+				for (int col = 0; col < BoardSize; col++)
+				{
+					// Check if both row and col numbers are even/odd at the same time.
+					if ((row % 2) == (col % 2))
+					{
+						gameField[row, col] = EmptyWhiteCell;
+					}
+					else
+					{
+						gameField[row, col] = EmptyBlackCell;
+					}
+				}
 			}
-        }
-    }
+		}
+
+
+		// Drawing method just for testing
+		//public static void drawBoard()
+		//{
+		//    for (int i = 0; i < BoardSize; i++)
+		//    {
+		//        for (int j = 0; j < BoardSize; j++)
+		//        {
+		//            System.Console.Write(gameField[i, j]);
+		//        }
+		//        System.Console.WriteLine();
+		//    }
+		//}
+	}
 }
