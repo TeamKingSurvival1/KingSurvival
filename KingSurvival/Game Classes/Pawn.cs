90d236e617476dd0cbb29aba649c4674fec0697f
dpenkova <dpenkova@gmail.com>
2014-07-17 01:18:27 +0300

    added const invalid pawn symbol 'K' and corresponding check in the constructor

--- KingSurvival/Game Classes/Pawn.cs
+++ KingSurvival/Game Classes/Pawn.cs
@@ -1,13 +1,20 @@
 ﻿namespace KingSurvival
 {
+    using System;
+
     internal class Pawn : Piece
     {
+        private const char InvalidPawnSymbol = 'K';        
         private Position position;
         private char pieceSymbol;
 
         public Pawn(char symbol, int initialX, int initialY)
         {
             position = new Position(initialX, initialY);
+            if (symbol == InvalidPawnSymbol)
+            {
+                throw new ArgumentException("Invalid pawn symbol!"); 
+            }
             this.pieceSymbol = symbol;
         }
 
