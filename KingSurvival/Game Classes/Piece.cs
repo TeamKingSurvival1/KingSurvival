a489403a944ed6039e697b1ba0641685bf018994
dpenkova <dpenkova@gmail.com>
2014-07-16 22:47:10 +0300

    added figure symbol abstract getter

--- KingSurvival/Game Classes/Piece.cs
+++ KingSurvival/Game Classes/Piece.cs
@@ -3,6 +3,7 @@
     internal abstract class Piece: IMoveable
     {
         internal abstract Position Position { get; }
+        internal abstract char Symbol { get; }
 
         public abstract void Move(int dirX, int dirY);
     }
