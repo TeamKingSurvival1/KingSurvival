a96b3840582da6311a02095084a362f2dd79a89b
dpenkova <dpenkova@gmail.com>
2014-07-16 22:45:49 +0300

    added figure symbol to constructor

--- KingSurvival/Game Classes/King.cs
+++ KingSurvival/Game Classes/King.cs
@@ -3,11 +3,22 @@
     using System;
     internal class King : Piece
     {
+        private const char KingsSymbol = 'K';
         private Position position;
+        private char pieceSymbol;
 
         public King(int initialX, int initialY)
         {
             position = new Position(initialX, initialY);
+            this.pieceSymbol = KingsSymbol;
+        }
+        
+        internal override char Symbol
+        {
+            get
+            {
+                return this.pieceSymbol;
+            }
         }
 
         internal override Position Position
