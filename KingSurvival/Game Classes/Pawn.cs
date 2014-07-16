6ebf7d011e49c2f295efc428a6fb54eba0014c25
dpenkova <dpenkova@gmail.com>
2014-07-16 22:46:36 +0300

    added figure symbol to constructor, implemented Move method

--- KingSurvival/Game Classes/Pawn.cs
+++ KingSurvival/Game Classes/Pawn.cs
@@ -3,12 +3,21 @@
     internal class Pawn : Piece
     {
         private Position position;
+        private char pieceSymbol;
 
-        public Pawn(int initialX, int initialY)
+        public Pawn(char symbol, int initialX, int initialY)
         {
             position = new Position(initialX, initialY);
+            this.pieceSymbol = symbol;
         }
 
+        internal override char Symbol
+        {
+            get
+            {
+                return this.pieceSymbol;
+            }
+        }
         internal override Position Position
         {
             get
@@ -19,6 +28,9 @@
 
         public override void Move(int dirX, int dirY)
         {
+            this.Position.Y += dirY;
+            this.Position.X += dirX;
+            return;
         }
     }
 }
