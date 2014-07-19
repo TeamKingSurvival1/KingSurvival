690c188fda97428e2205ecff8de4c4d45092665a
mrslvts <mrslvts@gmail.com>
2014-07-19 10:59:16 +0300

    including untracked files

---
+++ KingSurvival/Pawn.cs
@@ -0,0 +1,24 @@
+﻿namespace KingSurvival
+{
+    internal class Pawn : Piece
+    {
+        private Position position;
+
+        public Pawn(int x, int y)
+        {
+            position = new Position(x, y);
+        }
+
+        internal override Position Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+
+        internal override void Move()
+        {
+        }
+    }
+}
