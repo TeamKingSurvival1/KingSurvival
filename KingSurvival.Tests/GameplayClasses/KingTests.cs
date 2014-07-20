3cec64c3e93f912f03704fa71823674329426b8f
mrslvts <mrslvts@gmail.com>
2014-07-20 17:59:27 +0300

    added example tests for testing king's Move method

--- KingSurvival.Tests/GameplayClasses/KingTests.cs
+++ KingSurvival.Tests/GameplayClasses/KingTests.cs
@@ -3,6 +3,7 @@
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using KingSurvival.GameplayClasses;
+    using KingSurvival.Interfaces;
 
     [TestClass]
     public class KingTests
@@ -26,6 +27,46 @@
         public void TestingInitializationOfKingWithNegativeValuesOfXAndY()
         {
             King exampleKing = new King(-2, -5);
+        }
+
+        [TestMethod]
+        public void TestingKingsSymbol()
+        {
+            King exampleKing = new King(2, 5);
+            Assert.AreEqual('K', exampleKing.Symbol);
+        }
+
+
+        [TestMethod]
+        public void TestingKingsUpMovement()
+        {
+            King testKing = new King(2, 5);
+            testKing.Move(new Direction('u', 'l'));
+            Assert.AreEqual(4, testKing.Y);
+        }
+
+        [TestMethod]
+        public void TestingKingsDownMovement()
+        {
+            King testKing = new King(2, 5);
+            testKing.Move(new Direction('d', 'l'));
+            Assert.AreEqual(6, testKing.Y);
+        }
+
+        [TestMethod]
+        public void TestingKingsLeftMovement()
+        {
+            King testKing = new King(2, 5);
+            testKing.Move(new Direction('u', 'l'));
+            Assert.AreEqual(1, testKing.X);
+        }
+
+        [TestMethod]
+        public void TestingKingsRightMovement()
+        {
+            King testKing = new King(2, 5);
+            testKing.Move(new Direction('u', 'r'));
+            Assert.AreEqual(3, testKing.X);
         }
     }
 }
