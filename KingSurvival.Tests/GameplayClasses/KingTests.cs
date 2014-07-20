fd8c1a36282bb5204286e855b3dbe554906b2c7f
mrslvts <mrslvts@gmail.com>
2014-07-20 17:13:55 +0300

    added Test project; added few sample tests; It was enjoying!

---
+++ KingSurvival.Tests/GameplayClasses/KingTests.cs
@@ -0,0 +1,31 @@
+﻿namespace KingSurvival.Tests.GameplayClasses
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using KingSurvival.GameplayClasses;
+
+    [TestClass]
+    public class KingTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestingInitializationOfKingWithNegativeValueOfX()
+        {
+            King exampleKing = new King(-2, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestingInitializationOfKingWithNegativeValueOfY()
+        {
+            King exampleKing = new King(2, -5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestingInitializationOfKingWithNegativeValuesOfXAndY()
+        {
+            King exampleKing = new King(-2, -5);
+        }
+    }
+}
