619ebb6cf796d8f01070f23739f310ee73a40cd8
adimitrov93 <a.dimitrov93@gmail.com>
2014-07-27 17:12:31 +0300

    Added some more unit tests.

---
+++ KingSurvival.Tests/Engine/CommandTests.cs
@@ -0,0 +1,53 @@
+﻿namespace KingSurvival.Tests.Engine
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using KingSurvival.Engine;
+    using KingSurvival.Interfaces;
+    using KingSurvival.GameplayClasses;
+
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ParseTestThatMustThrowAnArgumentNullException()
+        {
+            Command.Parse("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ParseTestThatMustThrowAnArgumentOutOfRangeException()
+        {
+            Command.Parse("a");
+        }
+
+        [TestMethod]
+        public void ParseTestThatMustNotThrowAnException()
+        {
+            Assert.IsInstanceOfType(Command.Parse("KUR"), typeof(Command));
+        }
+
+        [TestMethod]
+        public void MoveDirectionTest()
+        {
+            Command command = Command.Parse("KUR");
+            IDirection expected = new Direction('U', 'R');
+            IDirection actual = command.MoveDirection;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TargetSymbolTest()
+        {
+            Command command = Command.Parse("KUR");
+            char expected = 'K';
+            char actual = command.TargetSymbol;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ KingSurvival.Tests/Engine/PieceFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿namespace KingSurvival.Tests.Engine
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using KingSurvival.Interfaces;
+    using KingSurvival.Engine;
+    using KingSurvival.GameplayClasses;
+
+    [TestClass]
+    public class PieceFactoryTests
+    {
+        [TestMethod]
+        public void CreateKingTest()
+        {
+            IPieceFactory factory = new PieceFactory();
+            King actual = factory.CreateKing();
+
+            Assert.IsInstanceOfType(actual, typeof(King));
+        }
+
+        [TestMethod]
+        public void CreatePawnsTest()
+        {
+            IPieceFactory factory = new PieceFactory();
+            IPiece[] pawns = factory.CreatePawns();
+            bool areAllPawns = true;
+
+            for (int i = 0; i < pawns.Length; i++)
+            {
+                if (pawns[i].GetType() != typeof(Pawn))
+                {
+                    areAllPawns = false;
+
+                    break;
+                }
+            }
+
+            Assert.IsTrue(areAllPawns);
+        }
+    }
+}
